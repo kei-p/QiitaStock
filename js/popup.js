9a11b3f4358b1ade1a31d1b4a31e7b2d6e47e314
Keisuke Ishizawa <k_ishizawa@sfidante.co.jp>
2015-12-04 12:27:26 +0900

    取得したstockの情報をmap

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,17 @@ $(function () {
   chrome.tabs.getSelected(null, function(tab) {
   });
 
+  var stocks = function (data) {
+    var stocks = []
+    data.map( function (v, i, a){
+      var stock = {}
+      stock.title = v.title
+      stock.url = v.url
+      stocks.push(stock)
+    });
+    return stocks;
+  }
+
   var loadStock = function() {
     var token = document.auth.token.value
     var userId = document.auth.userId.value
@@ -15,14 +26,7 @@ $(function () {
         'Authorization' : `Bearer ${token}`
       },
       success: function(d) {
-        var stocks = []
-        d.map( function (v, i, a){
-          var stock = {}
-          stock.title = v.title
-          stock.url = v.url
-          stocks.push(stock)
-        });
-        $('#message').text(JSON.stringify(stocks, null, 2));
+        $('#message').text(JSON.stringify(stocks(d), null, 2));
       },
       error: function(r, s, e){
         $('#message').text(JSON.stringify(e));
